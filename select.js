0504b93d35437f8835dd272b88794abda823e064
wanghailing <wanghailing@mail.fantuan.ca>
2020-12-23 14:17:30 +0800

    feat: 查找数据记录中的特定属性字段

--- select.js
+++ select.js
@@ -3,10 +3,20 @@ const {Blog, User} = require('./model')
 
 !(async function (){
   //查询一条记录
-  const zhangsan = await User.findOne({
-    where:{
-      userName: 'zhangsan'
-    }
-  })
-  console.log('zhangsan', zhangsan.dataValues)
+  // const zhangsan = await User.findOne({
+  //   where:{
+  //     userName: 'zhangsan'
+  //   }
+  // })
+  // console.log('zhangsan', zhangsan.dataValues)
+
+  //查找数据记录中的特定属性字段
+  //查找特定的列
+  // const zhangsanName = await User.findOne({
+  //   attributes: ['userName', 'nickName'],
+  //   where:{
+  //     userName: 'zhangsan'
+  //   }
+  // })
+  // console.log('zhangsanName', zhangsanName.dataValues)
 })()
