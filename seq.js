294024185cffd0b8163b7ab8de04a4187e038ad9
wanghailing <wanghailing@mail.fantuan.ca>
2020-12-24 17:21:00 +0800

    feat:增加连接池

--- seq.js
+++ seq.js
@@ -4,6 +4,11 @@ const conf = {
     host: 'localhost',
     dialect:'mysql'
 }
+conf.pool = {
+    max: 5, //连接池中最大的链接数量
+    min: 0, //最小
+    idle: 10000 // 如果一个链接池 10s 之内没有被使用，就释放
+}
 const seq = new Sequelize('weibo','root','Omg?110!ah,',conf)
 
 //测试数据库链接
