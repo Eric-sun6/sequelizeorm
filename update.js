1dcadf6551bad62ba1c7bbba22939453d8c18b62
wanghailing <wanghailing@mail.fantuan.ca>
2020-12-24 14:03:29 +0800

    feat:数据更新

---
+++ update.js
@@ -0,0 +1,14 @@
+const {Blog, User} = require('./model')
+
+!(async function(){
+  //更新数据
+  const updateRes = await User.update({
+    nickName: '张三11111',
+  }, {
+    where: {
+      userName: 'zhangsan'
+    }
+  })
+
+  console.log('updateRes', updateRes)
+})()
